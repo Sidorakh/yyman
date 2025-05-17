40dc4eefe615b368bf5fc656e2383e6fa919aa88
Andrew Rutherford <2681131+Sidorakh@users.noreply.github.com>
2025-05-18 00:20:29 +1000

    wait no i shoudl return not process.exit whatw as i thinking

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 export {YYOptionsMain,YYOptionsAndroid,YYOptionsWindows, YYOptionsHTML5, YYOptionsMac, YYOptionsOperaGX, YYOptionsFile, YYOptionsLinux} from "./types/options";
 
 if (require.main !== module) {
-    process.exit();
-}
+    return;
 
 import './cmdline';
