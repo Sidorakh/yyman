943e77783808528085668c13376ef170c0629036
Andrew Rutherford <2681131+Sidorakh@users.noreply.github.com>
2025-05-17 23:30:58 +1000

    Fixed package.json

--- index.ts
+++ index.ts
@@ -1,2 +1,7 @@
 export {YYOptionsMain,YYOptionsAndroid,YYOptionsWindows, YYOptionsHTML5, YYOptionsMac, YYOptionsOperaGX, YYOptionsFile, YYOptionsLinux} from "./types/options";
+
+if (require.main !== module) {
+    process.exit();
+}
+
 import './cmdline';
